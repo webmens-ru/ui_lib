257b521bc18705bb663a2e4ce86d927b2c9b583b
Alexander Rudetski <39997010+Haniley1@users.noreply.github.com>
2022-04-05 14:39:47 +0300

    Компонент Form
    Обновлена документация

--- stories/form/Form.stories.tsx
+++ stories/form/Form.stories.tsx
@@ -18,14 +18,18 @@ const Template: ComponentStory<typeof Form> = (args) => <Form {...args} />;
 
 export const BasicUsage = Template.bind({});
 BasicUsage.args = {
-  formConfig: {
-    fields: [
-      {type: 'input', name: 'test', label: 'Текстовое поле'},
-      {type: 'select', name: 'test1', label: 'Выпадающий список'}
-    ]
-  },
+  fields: [
+    { type: 'input', name: 'test', label: 'Текстовое поле' },
+    { type: 'select', name: 'test1', label: 'Выпадающий список', fieldParams: {
+      multiple: true,
+      data: [
+        {value: 1, title: 'Test1'},
+        {value: 2, title: 'Test2'},
+      ]
+    } }
+  ],
   validationRules: [
-    {type: "required", fields: ['test', 'test1'], rules: {message: 'Поле обязательно для заполнения'}},
-    {type: "integer", fields: ['test'], rules: {message: 'Значение должно быть числом!'}}
+    { type: "required", fields: ['test', 'test1'], rules: { message: 'Поле обязательно для заполнения' } },
+    { type: "integer", fields: ['test'], rules: { message: 'Значение должно быть числом!' } }
   ]
 };
