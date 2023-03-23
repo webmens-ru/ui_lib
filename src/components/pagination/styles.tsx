69fdac872c4897bf48cf751e7ef2e63f506016c4
Alexander Rudetski <39997010+Haniley1@users.noreply.github.com>
2023-03-23 11:09:51 +0300

    0.24.17

--- src/components/pagination/styles.tsx
+++ src/components/pagination/styles.tsx
@@ -4,12 +4,12 @@ export const PaginationContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  margin-top: 10px;
   padding: 15px 0;
 `
 
 export const PaginationItem = styled.span<{ active?: boolean, noBorder?: boolean }>`
   padding: 0 6px;
+  font-family: 'Open Sans';
   font-size: 11px;
   border-right: ${({ noBorder }) => noBorder ? "none" : "1px solid #cdcdcd"};
   color: ${({ active }) => active ? "#5c6470" : "#2067b0"};
