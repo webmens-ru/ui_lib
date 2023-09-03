cfe71c07c3f8025551cda9110781d0094a72fb62
Alexander Rudetski <39997010+Haniley1@users.noreply.github.com>
2023-09-04 00:12:42 +0300

    DatePicker: визуальные правки

--- src/components/date_picker/styles.tsx
+++ src/components/date_picker/styles.tsx
@@ -1,10 +1,11 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 type DatePickerContainerProps = {
     width?: string;
 }
 
 export const DatePickerContainer = styled.div`
+  position: relative;
   width: ${({width}: DatePickerContainerProps) => width ? width : '100%'};
   background: '#fff';
 `;
--- src/components/field/styles/Containers.tsx
+++ src/components/field/styles/Containers.tsx
@@ -1,14 +1,21 @@
 import styled from "styled-components";
-import SvgSprite from '../assets/main-ui-control.svg'
+import SvgSprite from '../assets/main-ui-control.svg';
 
 export const GreyBorderContainer = styled.div`
-  padding-left: 5px;
+  padding: 0 11px;
   position: relative;
   height: 40px;
   display: flex;
   align-items: center;
-  border: 1px solid #ccc;
+  border: 1px solid rgba(83, 92, 105, 0.2);
+  background: #fff;
   cursor: pointer;
+  transition: all 220ms linear 0s;
+
+  &:hover {
+    border-color: rgb(102, 175, 233);
+  }
+
   & * {
     color: #535c69;
     font-family: "Open Sans", sans-serif;
